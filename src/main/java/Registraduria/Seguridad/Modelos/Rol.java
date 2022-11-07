9261eefbc11c020b8124b3eb4011f7819159e7bd
Damlopezsu <113486617+Damlopezsu@users.noreply.github.com>
2022-11-06 20:34:04 -0600

    "creación de modelos"

---
+++ src/main/java/Registraduria/Seguridad/Modelos/Rol.java
@@ -0,0 +1,27 @@
+package main.java.Registraduria.Seguridad.Modelos;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+@Data
+@Document
+public class Rol {
+    @Id
+    private String id_rol;
+    private String nombre_rol;
+
+    public Rol(String nombre_rol) {
+        this.nombre_rol = nombre_rol;
+    }
+
+    public String getId_rol() {
+        return id_rol;
+    }
+
+    public String getNombre_rol() {
+        return nombre_rol;
+    }
+
+    public void setNombre_rol(String nombre_rol) {
+        this.nombre_rol = nombre_rol;
+    }
+}
